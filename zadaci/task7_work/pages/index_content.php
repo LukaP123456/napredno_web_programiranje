5b6460f4a0b8444243b17ea1866720a805041fdd
LukaPrcic12345 <bobsagott17@gmail.com>
2022-12-03 18:31:14 +0100

    task 7 done

--- zadaci/task7_work/pages/index_content.php
+++ zadaci/task7_work/pages/index_content.php
@@ -4,12 +4,12 @@ require_once './assets/functions.php';
 
 //require_once 'assets/autoload.php';
 
-require_once 'src/Web/Body.php';
-require_once 'src/Web/Header.php';
-require_once 'src/Web/Navigation.php';
-require_once 'src/Web/Html.php';
-require_once 'src/Web/Footer.php';
-require_once 'src/Web/Meta.php';
+require_once './src/Web/Body.php';
+require_once './src/Web/Header.php';
+require_once './src/Web/Navigation.php';
+require_once './src/Web/Html.php';
+require_once './src/Web/Footer.php';
+require_once './src/Web/Meta.php';
 
 use Web\{Body, Footer, Header, Html, Meta, Navigation};
 
